7d0ef932b035b429d9c649832de089cd1c6ff233
Ben Hope <bendef24@gmail.com>
2026-07-30 11:53:13 +0100

    feat(GRAN-113): add status and displayName to definitions

--- scripts/validate-feature-controls.js
+++ scripts/validate-feature-controls.js
@@ -35,7 +35,7 @@ const validate = () => {
       const featureControl = {
         name: yamlData.name ? yamlData.name.toUpperCase() : undefined,
         displayName: yamlData.displayName,
-        status: yamlData.status,
+        status: yamlData.status ? yamlData.status.toLowerCase() : undefined,
         type: yamlData.type,
         description: yamlData.description,
         scopes: yamlData.scopes,
